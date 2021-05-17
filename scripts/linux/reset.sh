c0d227c47a2ce0fcc949ad05c8582545e869b46a
Debrah Wyatt <debrahrwyatt@gmail.com>
2021-05-17 10:50:33 -0700

    reset.sh now also compiles code

--- scripts/linux/reset.sh
+++ scripts/linux/reset.sh
@@ -6,3 +6,7 @@ git pull
 
 chmod 777 /tmp/deb/seng440/scripts/linux/reset.sh
 chmod 777 /tmp/deb/seng440/scripts/linux/compile.sh
+
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S main.c
+arm-linux-gcc -static -o main.exe main.s
+lftp -c "open -u user4,q6coHjd7P arm; put main.exe"
