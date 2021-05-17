dd597701130ab66aab25b46df3e4d3ebda8b7687
Debrah Wyatt <debrahrwyatt@gmail.com>
2021-05-17 10:59:35 -0700

    Update reset.sh

--- scripts/linux/reset.sh
+++ scripts/linux/reset.sh
@@ -7,7 +7,7 @@ git pull
 chmod 777 /tmp/deb/seng440/scripts/linux/reset.sh
 chmod 777 /tmp/deb/seng440/scripts/linux/compile.sh
 
-if [ $1 -eq "-c" ]
+if [ "$1" -eq "-c" ]
 then
   /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/deb/seng440/main.c
   arm-linux-gcc -static -o /tmp/deb/seng440/main.exe /tmp/deb/seng440/main.s
