aa856be543c2f10745a6381cf6cbf385bb059abf
Debrah Wyatt <debrahrwyatt@gmail.com>
2021-05-17 10:49:50 -0700

    removed commented code

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-# git pull
+
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S main.c
 arm-linux-gcc -static -o main.exe main.s
 lftp -c "open -u user4,q6coHjd7P arm; put main.exe"
-# lftp user4@arm
-# q6coHjd7P
-# put main.exe
-# telnet arm
-# user4
-# q6coHjd7P
