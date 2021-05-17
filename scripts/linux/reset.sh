d871c476227364ce40180902a2e45853225c3e0a
Debrah Wyatt <debrahrwyatt@gmail.com>
2021-05-17 10:52:07 -0700

    Update reset.sh

--- scripts/linux/reset.sh
+++ scripts/linux/reset.sh
@@ -7,6 +7,6 @@ git pull
 chmod 777 /tmp/deb/seng440/scripts/linux/reset.sh
 chmod 777 /tmp/deb/seng440/scripts/linux/compile.sh
 
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S main.c
-arm-linux-gcc -static -o main.exe main.s
-lftp -c "open -u user4,q6coHjd7P arm; put main.exe"
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/deb/seng440/main.c
+arm-linux-gcc -static -o /tmp/deb/seng440/main.exe /tmp/deb/seng440/main.s
+lftp -c "open -u user4,q6coHjd7P arm; put /tmp/deb/seng440/main.exe"
