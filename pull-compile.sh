9b9b52189686e68180bc79dafa330ce83947d24c
DebrahWyatt <debrahrwyatt@gmail.com>
2021-05-16 18:36:18 -0700

    A test comment

--- pull-compile.sh
+++ pull-compile.sh
@@ -3,8 +3,8 @@ git pull
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S main.c
 arm-linux-gcc -static -o main.exe main.s
 lftp user4@arm
-q6coHjd7P
-put main.exe
-telnet arm
-user4
-q6coHjd7P
+# q6coHjd7P
+# put main.exe
+# telnet arm
+# user4
+# q6coHjd7P
