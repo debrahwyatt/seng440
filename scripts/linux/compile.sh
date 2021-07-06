3a36da0f729f45c1399b8b8652fbe083e840c890
DebrahWyatt <debrahrwyatt@gmail.com>
2021-07-05 19:24:31 -0700

    changed acting script directories

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /seng440/main.c
-arm-linux-gcc -static -o /seng440/main.exe /seng440/main.s
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/seng440/main.c
+arm-linux-gcc -static -o /tmp/seng440/main.exe /tmp/seng440/main.s
 lftp -c "open -u user4,q6coHjd7P arm; put main.exe"
