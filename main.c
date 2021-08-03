37bfaeee1476fb57dcd232721f17afd30663b248
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-03 13:49:48 -0700

    testing

--- main.c
+++ main.c
@@ -9,11 +9,7 @@
 //Global variables
 #define order 3
 int augmented_matrix[3][3*order];
-int matrix[3][3] = {    
-                        {3, 2, -4}, 
-                        {2, 3, 3}, 
-                        {5, -3, 1} 
-                    };
+
 
   ////////////////////////////
  /// FUNCTION DECLARATION ///
@@ -28,6 +24,13 @@ void printMatrix(int (*)[], int N);
 int main( void) {
     // ss = vadd_u32( aa, bb);
     printf("\n");
+    
+    //2x2 Matrix Inversion: [M]^-1 = 1/(a*d - b*c) * [M]
+    int matrix[3][3] = {    
+                        {3, 2, -4}, 
+                        {2, 3, 3}, 
+                        {5, -3, 1} 
+                    };
 
     //Determaine the length of the square matrix
     int N = sizeof(matrix) / sizeof(matrix[0]);
