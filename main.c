e13d25ca100bbc3d75d6bfe3286b14c787698d2a
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-03 10:55:31 -0700

    testing

--- main.c
+++ main.c
@@ -1,57 +1,55 @@
-// //   ////////////////
-// //  /// PACKAGES ///
-// // ////////////////
+  ////////////////
+ /// PACKAGES ///
+////////////////
 #include <stdio.h>
-// // // #include "arm_neon.h"
-// // // uint32x2_t aa, bb, ss;
-// // // -std=c99
+// #include "arm_neon.h"
+// uint32x2_t aa, bb, ss;
+// -std=c99
 
-// //   ////////////////////////////
-// //  /// FUNCTION DECLARATION ///
-// // ////////////////////////////
-// // void printMatrix(int (*)[], int N);
+  ////////////////////////////
+ /// FUNCTION DECLARATION ///
+////////////////////////////
+void printMatrix(int (*)[], int N);
 
 
-// //   ////////////////////
-// //  /// PROGRAM MAIN ///
-// // ////////////////////
+  ////////////////////
+ /// PROGRAM MAIN ///
+////////////////////
 
 int main( void) {
-    printf("Hello, World!\n");
+    // ss = vadd_u32( aa, bb);
+    printf("\n");
+    
+    //2x2 Matrix Inversion: [M]^-1 = 1/(a*d - b*c) * [M]
+    int M[2][2] = {
+                    {1, 2}, 
+                    {3, 4}
+                };
+
+    //Determaine the length of the square matrix
+    int N = sizeof(M) / sizeof(M[0]);
+    printf("Size of Matrix is: %dx%d\n", N, N);
+    
+    //Print the matrix
+    printMatrix(M, N);    
 }
-// //     // ss = vadd_u32( aa, bb);
-// //     printf("\n");
+
+
+  /////////////////
+ /// FUNCTIONS ///
+/////////////////
+
+//prints a matrix to terminal
+void printMatrix(int (*M)[2], int N) {
     
-// //     //2x2 Matrix Inversion: [M]^-1 = 1/(a*d - b*c) * [M]
-// //     int M[2][2] = {
-// //                     {1, 2}, 
-// //                     {3, 4}
-// //                 };
+    int i;
+    int j;
 
-// //     //Determaine the length of the square matrix
-// //     int N = sizeof(M) / sizeof(M[0]);
-// //     printf("Size of Matrix is: %dx%d\n", N, N);
-    
-// //     //Print the matrix
-// //     printMatrix(M, N);    
-// }
-
-
-// //   /////////////////
-// //  /// FUNCTIONS ///
-// // /////////////////
-
-// // //prints a matrix to terminal
-// // void printMatrix(int (*M)[2], int N) {
-    
-// //     int i;
-// //     int j;
-
-// //     for(i=0; i<N; i++){
-// //         for(j=0; j<N; j++){
-// //             printf("[%d]", M[i][j]);
-// //         }
-// //         printf("\n");
-// //     }
-// //     printf("\n");
-// // }
+    for(i=0; i<N; i++){
+        for(j=0; j<N; j++){
+            printf("[%d]", M[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
