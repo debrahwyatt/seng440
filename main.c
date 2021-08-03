49033a2b676ee30363c6d14bc259f34cee505410
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-03 13:36:29 -0700

    testing

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 // uint32x2_t aa, bb, ss;
 // -std=c99
 #define order 3
+int augmented_matrix[3][3*order]; // n+ 1
+
 
   ////////////////////////////
  /// FUNCTION DECLARATION ///
@@ -59,3 +61,26 @@ void printMatrix(int (*M)[2], int N) {
     printf("\n");
 }
 
+void augment_matrix(){
+    //Augmentation variables
+    int i = 0;
+    int j = 0;
+    int mat_i = 0;
+    int mat_j = order;
+
+    for(i = 0; i < order; i++){
+        for(j = 0; j < order*2; j++){
+            if( (i < order) && (j < order) ){
+                augmented_matrix[i][j] = matrix[i][j];
+            }
+            else if(i == mat_i && j == mat_j) {
+               augmented_matrix[i][j] = 1;
+               mat_i++;
+               mat_j++;
+            }
+            else{
+                augmented_matrix[i][j] = 0;
+            }
+        }
+    }
+}
