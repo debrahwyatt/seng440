caeb6c8d44a3f6ac46a5a6ee02c2a502f5a6b719
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-03 13:56:25 -0700

    testing

--- main.c
+++ main.c
@@ -61,30 +61,25 @@ void printMatrix(int (*M)[2], int N) {
     printf("\n");
 }
 
-void augment_matrix(){
+void augment_matrix(int (*M)[2]){
     //Augmentation variables
     int i = 0;
     int j = 0;
     int mat_i = 0;
     int mat_j = order;
-    // int matrix[3][3] = {    
-    //                     {3, 2, -4}, 
-    //                     {2, 3, 3}, 
-    //                     {5, -3, 1} 
-    //                 };
 
     for(i = 0; i < order; i++){
         for(j = 0; j < order*2; j++){
             if( (i < order) && (j < order) ){
-                // augmented_matrix[i][j] = matrix[i][j];
+                // augmented_matrix[i][j] = M[i][j];
             }
             else if(i == mat_i && j == mat_j) {
-               // augmented_matrix[i][j] = 1;
+               augmented_matrix[i][j] = 1;
                mat_i++;
                mat_j++;
             }
             else{
-                // augmented_matrix[i][j] = 0;
+                augmented_matrix[i][j] = 0;
             }
         }
     }
