f68237552f106ed91f35fde7d00c3e1e2e689043
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-04 12:15:49 -0700

    testing

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
   ////////////////////////////
  /// FUNCTION DECLARATION ///
 ////////////////////////////
-void printMatrix(int (*)[]);
+void printMatrix(int (*)[], int row, int col);
 
 
   ////////////////////
@@ -33,7 +33,7 @@ int main(void) {
 
 
     
-    printMatrix(matrix);    
+    printMatrix(matrix, row, col);    
 
 
     /////////////////////////////////////////////
@@ -63,10 +63,10 @@ int main(void) {
     }
     //////////////////////////////////////////////////
 
-    // row = sizeof(augmented_matrix) / sizeof(augmented_matrix[0]);
-    // col = (sizeof(augmented_matrix)/sizeof(augmented_matrix[0][0]))/row;
-    // printf("Augmented Matrix row size is: %d\n", row);
-    // printf("Augmented Matrix col size is: %d\n", col);
+    row = sizeof(augmented_matrix) / sizeof(augmented_matrix[0]);
+    col = (sizeof(augmented_matrix)/sizeof(augmented_matrix[0][0]))/row;
+    printf("Augmented Matrix row size is: %d\n", row);
+    printf("Augmented Matrix col size is: %d\n", col);
     // printf("Augmented Matrix [0][0] is: %d\n", matrix[0][0]);
     // printMatrix(augmented_matrix, row, col);    
     // printMatrix(augmented_matrix, row, col);    
@@ -79,7 +79,7 @@ int main(void) {
 /////////////////
 
 //prints a matrix to terminal
-void printMatrix(int (*matrix)[2]) {
+void printMatrix(int (*matrix)[2], int row, int col) {
     
     //increment variables
     unsigned char x;
@@ -87,10 +87,7 @@ void printMatrix(int (*matrix)[2]) {
 
     //Determaine the column and row length of the matrix
     unsigned char row = sizeof(matrix) / sizeof(matrix[0]);
-    // unsigned char col = (sizeof(matrix)/sizeof(matrix[0][0]))/row;
-
-    // unsigned char row = 3;
-    unsigned char col = 3;
+    unsigned char col = (sizeof(matrix)/sizeof(matrix[0][0]))/row;
 
     printf("Size of Matrix is: %dx%d\n", row, col);
 
