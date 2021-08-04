ba88dd30a913f9952780be1fee0b34d7efe8536e
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-04 12:36:28 -0700

    testing

--- main.c
+++ main.c
@@ -45,7 +45,7 @@ int main(void) {
     unsigned char mat_i = 0;
     unsigned char mat_j = order;
 
-    int augmented_matrix[3][3*order];
+    int augmented_matrix[3][order*2];
 
     for(i = 0; i < order; i++){
         for(j = 0; j < order*2; j++){
