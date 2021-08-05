597e7cfc98261164038cedcf040fbc8cfad38324
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-05 13:34:48 -0700

    testing

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -2,4 +2,4 @@
 
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/dev/seng440/main.c
 arm-linux-gcc -static -o /tmp/dev/seng440/main.exe /tmp/dev/seng440/main.s
-lftp -c "open -u user4,q6coHjd7P arm; put main.exe"
+lftp -c "open -u user3,q6coHjd7P arm; put main.exe"
