2686dc8bd7a72ea7aab434962bb3383ad35e703b
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-11 12:21:45 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -67,10 +67,10 @@ void main(){
     // }
 
 
-    int16_t A[] = {1, 2, 1, 0, 3, 4, 0, 1};
+    int16_t B[] = {1, 2, 1, 0, 3, 4, 0, 1};
 
-    int16x4_t A0;
-    int16x4_t A1;
+    int16x4_t B0;
+    int16x4_t B1;
 
     int16x4_t b = {1, 2, 3 ,4 };
     int16x4_t d = {2,2,2,2};
@@ -88,18 +88,18 @@ void main(){
         multiply subtract operation
     */
 
-    A0 = vld1_s16(A);
-    A1 = vld1_s16(A+4);
+    B0 = vld1_s16(B);
+    B1 = vld1_s16(B+4);
 
-    vst1_s16(C, A0);
-    vst1_s16(C+4, A1);
+    vst1_s16(C, B0);
+    vst1_s16(C+4, B1);
 
     int16x4_t f = vmls_s16(b,d,e); // v3, v1, v2 = v3 - v1 * v2
 
     int16_t M[] = {0, 0, 0, 0};
     vst1_s16(M, f);
-    vst1_s16(C, A0);
-    vst1_s16(C+4, A1);
+    vst1_s16(C, B0);
+    vst1_s16(C+4, B1);
 
     for(int i = 0; i < 4; i++){
         printf("M: %i\n",  M[i]);
