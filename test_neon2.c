8f15523fc741f75d9a04c99838c46ff0158b0b25
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-10 17:33:06 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -67,36 +67,31 @@ void main(){
     // }
 
 
-    // int16x4_t b = {1, 2, 3 ,4 };
-    // int16x4_t d = {2,2,2,2};
-    // int16x4_t e = {3,3,3,3};
+    int16x4_t b = {1, 2, 3 ,4 };
+    int16x4_t d = {2,2,2,2};
+    int16x4_t e = {3,3,3,3};
 
     // rows of the augmented matrix b
-   // printf("A: \n", *A);
+    printf("A: \n", *A);
 
-    // int16_t C[] = {0, 0, 0, 0, 0 ,0 ,0, 0};
+    int16_t C[] = {0, 0, 0, 0, 0 ,0 ,0, 0};
 
-  //  int16x4_t f;
-
-    //vmls_s16
+    int16x4_t f;
+    vmls_s16
     /*
         multiply subtract operation
     */
 
-    // A0 = vld1_s16(A);
-    // A1 = vld1_s16(A+4);
+    A0 = vld1_s16(A);
+    A1 = vld1_s16(A+4);
 
-    // vst1_s16(C, A0);
-    // vst1_s16(C+4, A1);
+    vst1_s16(C, A0);
+    vst1_s16(C+4, A1);
 
-    // int16x4_t f = vmls_s16(b,d,e); // v3, v1, v2 = v3 - v1 * v2
+    int16x4_t f = vmls_s16(b,d,e); // v3, v1, v2 = v3 - v1 * v2
 
-    // int16_t M[] = {0, 0, 0, 0};
-    // vst1_s16(M, f);
-    // vst1_s16(C, A0);
-    // vst1_s16(C+4, A1);
-
-    // for(int i = 0; i < 4; i++){
-    //     printf("M: %i\n",  M[i]);
-    // }
+    int16_t M[] = {0, 0, 0, 0};
+    vst1_s16(M, f);
+    vst1_s16(C, A0);
+    vst1_s16(C+4, A1);
 }
