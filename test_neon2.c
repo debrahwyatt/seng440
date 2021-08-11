edad450b2cbd1450ff7782a714ea0e185e4963ee
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-10 17:33:50 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -67,17 +67,23 @@ void main(){
     // }
 
 
+    int16_t A[] = {1, 2, 1, 0, 3, 4, 0, 1};
+
+    int16x4_t A0;
+    int16x4_t A1;
+
     int16x4_t b = {1, 2, 3 ,4 };
     int16x4_t d = {2,2,2,2};
     int16x4_t e = {3,3,3,3};
 
     // rows of the augmented matrix b
-    printf("A: \n", *A);
+   // printf("A: \n", *A);
 
     int16_t C[] = {0, 0, 0, 0, 0 ,0 ,0, 0};
 
-    int16x4_t f;
-    vmls_s16
+  //  int16x4_t f;
+
+    //vmls_s16
     /*
         multiply subtract operation
     */
@@ -94,4 +100,8 @@ void main(){
     vst1_s16(M, f);
     vst1_s16(C, A0);
     vst1_s16(C+4, A1);
+
+    for(int i = 0; i < 4; i++){
+        printf("M: %i\n",  M[i]);
+    }
 }
