f43e88a6f4f8374714e21e4a578d9238dca10086
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-13 11:20:20 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -89,8 +89,8 @@ void main(){
      ///     PIVOT    ///
     ////////////////////
     
-    int first_op;
-    int second_op;    
+    int16_t first_op;
+    int16_t second_op;    
 
     for(k = 0; k < n; k++){
         first_op = A[ k*m + k ];
