f380fe1a1d05a376fb6763769a56f0db1bc37c32
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-14 10:50:47 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -129,7 +129,7 @@ void main(){
 
     int32_t a = 6570;
     int32_t b = (540) << 1;
-    int32_t c = (b / a);
+    int32_t c = (b / a) << 4;
     printf("%i \n\n", a);
     printf("%i \n\n", b);
 
