5e6db7ae45b25910b7bcae88be6fef091e00cb88
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-14 11:08:27 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -127,8 +127,8 @@ void main(){
     //Prints the augmented matrix
     printMatrix( I, n, i, j );
 
-    int32_t a = 4;
-    int32_t b = 2;
+    int32_t a = 10;
+    int32_t b = 5;
     int32_t c = (((a)<<1) / (b)<<1);
     printf("%i \n\n", a);
     printf("%i \n\n", b);
