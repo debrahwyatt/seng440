50ae095e3edf7f025fb2fafba0046802e84a2e65
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-15 12:16:59 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -133,11 +133,11 @@ void main(){
     for(i = 0; i < n ; i++){ 
         for(j = n; j < m; j++){
             printf("%i \n\n", A[ i*m + j ]);
-            printf("%i \n\n", A[ i*n + i ]);
+            printf("%i \n\n", A[ i*m + i ]);
             if(A[ i*n + i ] == 0){
                 continue;
             }
-            A[ i*m + j ] = (A[ i*m + j ]  / A[ i*n + i ]) ;
+            A[ i*m + j ] = (A[ i*m + j ]  / A[ i*m + i ]) ;
         }
     }
 
