72fd5b1af259328a848c890aa9c556da38e69a8e
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-15 12:22:58 -0700

    testing

--- test_neon2.c
+++ test_neon2.c
@@ -72,6 +72,14 @@ void pivot( int16_t *A, int8_t m, int8_t i, int8_t j, int8_t k ) {
     }
 }
 
+void division( int16_t *A, int8_t m, int8_t i, int8_t j ) {
+    for(i = 0; i < n ; i++){ 
+        for(j = n; j < m; j++){
+            A[ i*m + j ] = ((A[ i*m + j ] << scale ) / A[ i*m + i ]) ;
+        }
+    }
+}
+
 void collectInverse( int16_t *I, int16_t *A, int16_t m, int8_t i, int8_t j, int8_t k ) {
     for(i = 0; i < n ; i++){ 
         k = 0;
@@ -85,9 +93,7 @@ void collectInverse( int16_t *I, int16_t *A, int16_t m, int8_t i, int8_t j, int8
 void printInverse( int16_t *I, int8_t m, int8_t i, int8_t j ) {
     for(i = 0; i < n; i++){
         for(j = 0; j < m; j++){
-            // printf("%i ",  I[i*m + j]);
             printf("%f ", (float)(I[i*m + j])/pow(2, scale));
-
         }
         printf("\n");
     }
@@ -123,44 +129,8 @@ void main(){
     pivot( A, m, i, j, k );
     printMatrix( A, m, i, j );
 
-    // Real -> integer
-    // Integer -> real
-      ////////////////////
-     ///   DIVISION   ///
-    ////////////////////
-
-    for(i = 0; i < n ; i++){ 
-        for(j = n; j < m; j++){
-            // printf("%i \n\n", A[ i*m + j ]);
-            // printf("%i \n\n", A[ i*m + i ]);
-            // if(A[ i*n + i ] == 0){
-            //     continue;
-            // }
-            A[ i*m + j ] = ((A[ i*m + j ] << scale ) / A[ i*m + i ]) ;
-        }
-    }
+    division( A, m, i, j );
 
     collectInverse( I, A, m, i, j, k );
     printInverse( I, n, i, j );
-
-    //Prints the augmented matrix
-    // printMatrix( I, n, i, j );
-
-    // 0000 0000 0000 0000
-
-    // 0000 0000 0001 0101
-    // int16_t a = 21;
-
-    // 0000 0000 0000 0010
-    // int16_t b = 2;
-
-    // a = a << scale;
-    // 0001 0101 0000 0000
-
-    // int16_t e = (c << 16) >> 16;
-    // 1000 0000 0000 0000 
-    // printf("%i \n\n", a);
-    // printf("%i \n\n", b);
-    // printf("%f \n\n", (float)(a/b)/pow(2, scale));
-
 }
