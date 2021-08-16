6f66a3a9827333b6b9334d074a7ec53597130c4e
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-16 10:36:51 -0700

    testing

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -2,4 +2,4 @@
 
 /opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/dev/seng440/neon.c
 arm-linux-gcc -static -o /tmp/dev/seng440/neon.exe /tmp/dev/seng440/neon.s
-lftp -c "open -u user3,q6coHjd7P arm; put main.exe"
+lftp -c "open -u user3,q6coHjd7P arm; put neon.exe"
