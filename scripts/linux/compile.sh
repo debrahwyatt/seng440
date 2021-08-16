669cee22d6f3af35d6234f60d56d31cc3106868b
DebrahWyatt <debrahrwyatt@gmail.com>
2021-08-16 10:52:56 -0700

    testing

--- scripts/linux/compile.sh
+++ scripts/linux/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/dev/seng440/mat_inv.c
-arm-linux-gcc -static -o /tmp/dev/seng440/mat_inv.exe /tmp/dev/seng440/mat_inv.s
-lftp -c "open -u user3,q6coHjd7P arm; put mat_inv.exe"
+/opt/arm/4.3.2/bin/arm-linux-gcc -mfloat-abi=softfp -mfpu=neon -static -O3 -S /tmp/dev/seng440/main.c
+arm-linux-gcc -static -o /tmp/dev/seng440/main.exe /tmp/dev/seng440/main.s
+lftp -c "open -u user3,q6coHjd7P arm; put main.exe"
